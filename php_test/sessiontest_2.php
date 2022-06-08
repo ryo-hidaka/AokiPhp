02d5da3aa4f21b79e8814d325f9b0b1b8d58c012
ryo-hidaka <percussion665@gmail.com>
2022-06-08 19:01:32 +0900

    セッションと高度な関数

---
+++ php_test/sessiontest_2.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo "セッションを破棄しました";
+
+    $_SESSION = [];
+
+    if(!isset($_COOKIE["PHPSESSID"])){
+        setcookie("PHPSESSID", "", time(), -1800, "/");
+    } 
+
+    session_destroy();
+    
+    echo "<pre>";
+    var_dump($_SESSION);
+    echo "</pre>";
+
+    echo "<pre>";
+    var_dump($_COOKIE);
+    echo "</pre>";
+
+    
+    ?>
+</body>
+</html>
